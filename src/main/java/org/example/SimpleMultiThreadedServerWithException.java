a3c713425e32dc30b4d5d2802b50aa800373ca2b
Amimul Ehsan <58260966+Amimul1234@users.noreply.github.com>
2023-06-04 03:37:46 +0600

    Added more solid examples.

--- src/main/java/org/example/SimpleMultiThreadedServerWithException.java
+++ src/main/java/org/example/SimpleMultiThreadedServerWithException.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -24,10 +22,7 @@ public class SimpleMultiThreadedServerWithException {
     }
 
     private static void handleRequest(Socket clientSocket) throws IOException {
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
-            String request = in.readLine();
-
+        try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
             // Process the request
             String response = "HTTP/1.1 200 OK\r\n" +
                     "Content-Type: text/html\r\n" +
