caf2b6d7b22a11451d159238cfbe4bc585231715
Amimul Ehsan <58260966+Amimul1234@users.noreply.github.com>
2023-06-04 16:38:51 +0600

    Added more solid examples.

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -2,5 +2,9 @@
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
-S
-#src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
+C
+docs/mvn_plugin.txt,2\a\2aa3ecc341c1b9f85f1e82c692c2f9184b434964
+z
+Jsrc/main/java/org/example/day1/SimpleMultiThreadedServerWithException.java,1\6\164e7a490a967e7f547bd28f5c599865699a832e
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
--- src/main/java/org/example/day1/SimpleMultiThreadedServer.java
+++ src/main/java/org/example/day1/SimpleMultiThreadedServer.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 public class SimpleMultiThreadedServer {
 
     public static void main(String[] args) {
-        int port = 8080;
+        int port = 9095;
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server listening on port " + port);
@@ -24,13 +24,28 @@ public class SimpleMultiThreadedServer {
     private static void handleRequest(Socket clientSocket) throws IOException {
         try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
 
+            if(out.equals("/ttt./")){
+                String response = "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "\r\n" +
+                        "<html><body>" +
+                        "<h1>ttt</h1>" +
+                        "</body></html>";
+            }else if(out.equals("/aaa")){
+                String response = "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "\r\n" +
+                        "<html><body>" +
+                        "<h1>aaaaa</h1>" +
+                        "</body></html>";
+            }
 
             // Process the request
             String response = "HTTP/1.1 200 OK\r\n" +
                     "Content-Type: text/html\r\n" +
                     "\r\n" +
                     "<html><body>" +
-                    "<h1>Hello, World!</h1>" +
+                    "<h1>ABCD</h1>" +
                     "</body></html>";
 
             out.println(response);
